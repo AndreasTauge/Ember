c15efd8298ba84ee68252ac5eab1647a81306ffe
AndreasTauge <andreas@tauge.no>
2026-07-18 18:33:21 +0200

    Added a toy example to test autograd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use ember::autograd;
-use ember::tensor;
+use ember::autograd::tensor_autodiff::Graph;
+use ember::tensor::Tensor;
 
 fn main() {
-    println!("Hello, world!");
+    let mut graph = Graph::new(vec![]);
+    let x = graph.tensor(Tensor::new(vec![1.0, 2.0, 3.0], vec![3]));
+    let y = graph.mul(x, x);
+    let loss = graph.sum(y);
+    graph.backward();
+    println!("x grad = {:?}", graph.grad(x).values());
 }
